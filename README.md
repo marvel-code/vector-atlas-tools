92d0f0b9c3d0e9ca506329a4685550bb2e034551
Rodion Kostin <itrodion@gmail.com>
2022-03-09 17:53:39 +0300

    create bmp image

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Example:
 ```
 python27 ./parser.py ./myfont.ttf
 ```
+
+flags:
+--show - shows result bmp image
--- parser.py
+++ parser.py
@@ -2,24 +2,26 @@
 """
     sys.argv[1] - path to font file.
 """
-from itertools import count
+import json
 from ttfquery import describe
 import ttfquery.glyph as glyph
-from ttfquery.glyphquery import glyphName
+import ttfquery.glyphquery as glyphquery
 import sys
+from PIL import Image
 
 # Parses all glyphs in font if true, else parses glyphs from CUSTOM_PARSING_GLYPHS
-ALL_GLYPHS = False
+ALL_GLYPHS = True
 # If ALL_GLYPHS is False, fetch glyphs from CUSTOM_PARSING_GLYPHS
-CUSTOM_PARSING_GLYPHS = u')'
+CUSTOM_PARSING_GLYPHS = u'='
 
 flags = filter(lambda x: x[0] == '-', sys.argv[1:])
 font_path = sys.argv[1]
 font = describe.openFont(font_path)
 glyph_names = font.getGlyphNames() \
-    if ALL_GLYPHS else [glyphName(font, gn) for gn in CUSTOM_PARSING_GLYPHS]
+    if ALL_GLYPHS else [glyphquery.glyphName(font, gn) for gn in CUSTOM_PARSING_GLYPHS]
 
-glyphs_beziers = {}
+glyph_infos = {}
+max_points_count = 0
 for glyph_name in glyph_names:
     if not ALL_GLYPHS:
         print(glyph_name)
@@ -37,8 +39,23 @@ for glyph_name in glyph_names:
             points.append(fpoint)
             last_fpoint = fpoint
         uncompressed_contours.append(points)
-    glyphs_beziers[glyph_name] = uncompressed_contours
+    glyph_infos[glyph_name] = {
+        'contours': uncompressed_contours,
+        'width': glyphquery.width(font, glyph_name),
+    }
+    points_count = sum(map(lambda x: sum(map(lambda y: len(y), x)), uncompressed_contours))
+    max_points_count = max(max_points_count, points_count)
 
-print(glyphs_beziers)
-    
+print('height', glyphquery.charHeight(font))
+#print(glyph_infos)
+print(max_points_count)
+print(len(glyph_infos))
 
+with open('atlas.bmp', 'w') as f:
+    img = Image.new('RGBA', (255, 255), (255, 0, 0, 0))
+    pixels = img.load()
+    for i in range(img.size[0]):    # For every pixel:
+        for j in range(img.size[1]):
+            pixels[i,j] = (i, j, 100) # Set the colour accordingly
+    if '--show' in flags:
+        img.show()
