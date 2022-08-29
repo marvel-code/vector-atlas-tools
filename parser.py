bc2da99529cdc122ef3e163b762e068511679965
Rodion Kostin <itrodion@gmail.com>
2022-08-29 15:45:19 +0300

    success parsing

--- parser.py
+++ parser.py
@@ -2,19 +2,15 @@
 """
     sys.argv[1] - path to font file.
 """
-from cmath import sqrt
 from copy import copy
-import math
-from genericpath import exists
 import json
-from math import ceil
-from numpy import sort
 from ttfquery import describe
 import ttfquery.glyph as glyph
 import ttfquery.glyphquery as glyphquery
 import sys
 import os
 from PIL import Image
+from utils import *
 
 ON_CURVE = 1
 OFF_CURVE = 0
@@ -25,164 +21,33 @@ CUSTOM_PARSING_GLYPHS = u' `1234567890-=~!@#$%^&*()_+qwertyuiop[]QWERTYUIOP{}|as
 #CUSTOM_PARSING_GLYPHS = u'еabc'
 DIST_DIR = "dist"
 
+ATLAS_WIDTH = 256
+ATLAS_HEIGHT = 128
+
 print("Glyphs: " + (CUSTOM_PARSING_GLYPHS if not ALL_GLYPHS else 'all'))
 
-def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
-    '''
-    Python 2 implementation of Python 3.5 math.isclose()
-    https://hg.python.org/cpython/file/tip/Modules/mathmodule.c#l1993
-    '''
-    # sanity check on the inputs
-    if rel_tol < 0 or abs_tol < 0:
-        raise ValueError("tolerances must be non-negative")
+# etc
 
-    # short circuit exact equality -- needed to catch two infinities of
-    # the same sign. And perhaps speeds things up a bit sometimes.
-    if a == b:
-        return True
-
-    # This catches the case of two infinities of opposite sign, or
-    # one infinity and one finite number. Two infinities of opposite
-    # sign would otherwise have an infinite relative tolerance.
-    # Two infinities of the same sign are caught by the equality check
-    # above.
-    if math.isinf(a) or math.isinf(b):
-        return False
-
-    # now do the regular computation
-    # this is essentially the "weak" test from the Boost library
-    diff = math.fabs(b - a)
-    result = (((diff <= math.fabs(rel_tol * b)) or
-               (diff <= math.fabs(rel_tol * a))) or
-              (diff <= abs_tol))
-    return result
-
-def normalizeFPoint(fpoint, size):
-    return ((float(fpoint[0][0]) / size[0], float(fpoint[0][1]) / size[1]), fpoint[1])
-
-def pointInside(point, rectangle):
+def fetch_beziers(contours, char_size):
     """
-    @param rectangle (x0,y0,x1,y1) x0,y0 left-bottom. x1,y1 right-top.
+    Начало координат находится в левом нижнем углу.
+    Оси направлены вверх и направо.
     """
-    return point[0] >= rectangle[0] and point[0] <= rectangle[2] and \
-        point[1] >= rectangle[1] and point[1] <= rectangle[3]
-
-def insideSection(t, t1, t2):
-    return t >= t1 and t <= t2
-    
-def fetchBezierCross(bezier, componentIndex, componentValue):
-    """
-    B(t) = (1-t)^2*p0 + 2(1-t)t*p1 + t^2*p2
-    B(t) = (p0-2p1+p2)tt + 2(p1-p0)t + p0
-
-    component is x or y.
-    componentIndex == 0 is x.
-    componentIndex == 1 is y.
-    @return boolean
-    """
-    c0 = bezier[0][componentIndex]
-    c1 = bezier[1][componentIndex]
-    c2 = bezier[2][componentIndex]
-    a = c0 - 2 * c1 + c2
-    b = c1 - c0
-    c = c0 - componentValue
-    if isclose(a, 0):
-        # Linear
-        if isclose(b, 0):
-            return []
-        t = c / b / 2
-        return [t] if insideSection(t, 0, 1) else []
-    
-    D = b ** 2 - a * c
-    if D < 0:
-        return []
-
-    t1 = (-b - sqrt(D)) / a
-    t2 = (-b + sqrt(D)) / a
-    solution = []
-    if insideSection(t1.real, 0, 1):
-        solution.append(t1.real)
-    if insideSection(t2.real, 0, 1):
-        solution.append(t2.real)
-    return solution
-
-def calcBezier(bezier, t):
-    def calcComponentBezier(c0, c1, c2, t):
-        return (1-t)**2*c0 + 2*(1-t)*t*c1 + t**2*c2
-
-    p0, p1, p2 = bezier
-    return (
-        calcComponentBezier(p0[0], p1[0], p2[0], t),
-        calcComponentBezier(p0[1], p1[1], p2[1], t)
-    )
-
-def bezierInsideCell(bezier, cellRectangle):
-    x1 = cellRectangle[0]
-    y1 = cellRectangle[1]
-    x2 = cellRectangle[2]
-    y2 = cellRectangle[3]
-    x1t = fetchBezierCross(bezier, 0, cellRectangle[0])
-    y1t = fetchBezierCross(bezier, 1, cellRectangle[1])
-    x2t = fetchBezierCross(bezier, 0, cellRectangle[2])
-    y2t = fetchBezierCross(bezier, 1, cellRectangle[3])
-    return pointInside(bezier[0], cellRectangle) or pointInside(bezier[2], cellRectangle)\
-        or any(map(lambda t: insideSection(calcBezier(bezier, t)[1], y1, y2), x1t))\
-        or any(map(lambda t: insideSection(calcBezier(bezier, t)[0], x1, x2), y1t))\
-        or any(map(lambda t: insideSection(calcBezier(bezier, t)[1], y1, y2), x2t))\
-        or any(map(lambda t: insideSection(calcBezier(bezier, t)[0], x1, x2), y2t))
-    
-def avrPoint(p1, p2):
-    x0 = float(p1[0] + p2[0]) / 2
-    y0 = float(p1[1] + p2[1]) / 2
-    return (x0, y0)
-
-def makeGrid(beziers):
-    """
-    @return number[][];
-    """
-    MAX_GRID_SIZE = 32
-    MAX_CELL_BEZIERS = 4
-    grid = None
-    for gridSize in range(2, MAX_GRID_SIZE):
-        grid = []
-        skip = False
-        for i in range(gridSize):
-            row = []
-            for j in range(gridSize):
-                cellRectangle = (1. / gridSize * i, 1. / gridSize * j, 1. / gridSize * (i + 1), 1. / gridSize * (j + 1))
-                cell = []
-                for bi, b in enumerate(beziers):
-                    if bezierInsideCell(b, cellRectangle):
-                        cell.append(bi)
-                row.append(cell)
-                if len(cell) > MAX_CELL_BEZIERS:
-                    skip = True
-                    break
-            if skip:
-                break
-            grid.append(row)
-        if not skip:
-            break
-        if gridSize == MAX_GRID_SIZE - 1:
-            raise Exception("Max grid size enriched")
-    return grid
-
-def fetchBeziers(contours, charSize):
     beziers = []
     for contour in contours:
-        # Contour points
+        # Fetch contour points
         points = []
         last_fpoint = contour[-1]
         for i, fpoint in enumerate(contour):
             if (last_fpoint[1] == fpoint[1] and fpoint[1] == OFF_CURVE):
-                fpoint_ = (avrPoint(fpoint[0], last_fpoint[0]), ON_CURVE)
-                points.append(normalizeFPoint(fpoint_, charSize))
+                fpoint_ = (avrpoint(fpoint[0], last_fpoint[0]), ON_CURVE)
+                points.append(normalize_fpoint(fpoint_, char_size))
             if i > 0 and (last_fpoint[1] == fpoint[1] and fpoint[1] == ON_CURVE):
-                fpoint_ = (avrPoint(fpoint[0], last_fpoint[0]), OFF_CURVE)
-                points.append(normalizeFPoint(fpoint_, charSize))
-            points.append(normalizeFPoint(fpoint, charSize))
+                fpoint_ = (avrpoint(fpoint[0], last_fpoint[0]), OFF_CURVE)
+                points.append(normalize_fpoint(fpoint_, char_size))
+            points.append(normalize_fpoint(fpoint, char_size))
             last_fpoint = fpoint
-        # Contour beziers
+        # Group contour points into beziers
         for i in range(int((len(points) + 2) / 3)):
             bezier = []
             for j in range(3):
@@ -190,17 +55,89 @@ def fetchBeziers(contours, charSize):
             beziers.append(bezier)
     return beziers
     
+def make_grid(beziers):
+    """
+    (i,j) cell boundaries are (d*j, d*i, d*(j + 1), d*(i + 1)), where `d` is `1/gridSize`.
+    Начальная ячейка находится в нижнем левом углу.
+    (i,j) соответствует i строке, j столбцу.
+    @return number[][];
+    """
+    MAX_GRID_SIZE = 32
+    MAX_CELL_BEZIERS = 4
+    grid = None
+    for gridsize in range(2, MAX_GRID_SIZE):
+        grid = []
+        skip = False
+        d = 1. / gridsize
+        for i in range(gridsize):
+            row = []
+            for j in range(gridsize):
+                cellRectangle = (d*j, d*i, d*(j + 1), d*(i + 1)) # (x1,y1,x2,y2)
+                cell = []
+                for bi, b in enumerate(beziers):
+                    if is_bezier_crossing_cell(b, cellRectangle):
+                        if len(cell) == MAX_CELL_BEZIERS:
+                            skip = True
+                            break
+                        cell.append(bi)
+                if skip:
+                    break
+                row.append(cell)
+            if skip:
+                break
+            grid.append(row)
+        if not skip:
+            break
+        if gridsize == MAX_GRID_SIZE - 1:
+            raise Exception("Max grid size enriched")
+    return grid
+
+def copy_grid(grid, atlas, coord):
+    """Copy grid into atlas with `coord` offset."""
+    if len(grid) + coord[1] >= len(atlas) or len(grid) + coord[0] >= len(atlas[0]):
+        print(len(grid), len(atlas), coord)
+        raise Exception('Grid exceeded atlas')
+    grid_size = len(grid)
+    for rowi in range(grid_size):
+        for coli in range(grid_size):
+            cell = grid[rowi][coli] # todo: check
+            while len(cell) < 4:
+                cell.append(0)
+            atlas[coord[1] + rowi][coord[0] + coli] = cell
+
+def copy_matrix(matrix, atlas, coord):
+    """Copy matrix into atlas by rows with `coord` offset."""
+    for r in range(len(matrix)):
+        for c in range(len(matrix[r])):
+            atlas[coord[1] + r][coord[0] + c] = matrix[r][c]
+
+def convert_beziers_to_pixels(beziers):
+    if len(beziers) == 0:
+        return []
+    pixels = []
+    points = compress_beziers(beziers)
+    assert(len(points) % 2 == 1, 'Points length is not odd: {0}'.format(len(points)))
+    for p in points:
+        x = int(p[0] * 0xff) # todo: check native shader implementation
+        y = int(p[1] * 0xff)
+        if len(pixels) > 0 and len(pixels[-1]) == 2:
+            pixels[-1] += [x, y]
+        else:
+            pixels.append([x, y])
+    pixels[-1] += [0,0] # Because points is odd length, needs to be aligned to 4 bytes
+    return pixels
+
 
 flags = filter(lambda x: x[0] == '-', sys.argv[1:])
 font_path = sys.argv[1]
 font = describe.openFont(font_path)
-charHeight = glyphquery.charHeight(font)
-glyphNameMap = font['cmap'].getcmap(*describe.guessEncoding(font)).cmap # char -> glyphName
-charMap = dict([(value, key) for key, value in glyphNameMap.items()]) # glyphName -> char
+charheight = glyphquery.charHeight(font)
+glyphname_map = font['cmap'].getcmap(*describe.guessEncoding(font)).cmap # char -> glyphName
+charmap = dict([(value, key) for key, value in glyphname_map.items()]) # glyphName -> char
 glyph_names = font.getGlyphNames() \
     if ALL_GLYPHS else [glyphquery.glyphName(font, gn) for gn in CUSTOM_PARSING_GLYPHS]
 
-glyphInfos = []
+glyph_infos = []
 stat = {
     'maxPointsCount': (0, None),
     'maxGridSize': (0, None),
@@ -208,21 +145,21 @@ stat = {
 for glyph_name in glyph_names:
     g = glyph.Glyph(glyph_name)
     contours = g.calculateContours(font)
-    charWidth = glyphquery.width(font, glyph_name)
-    if charWidth == 0 or glyph_name not in charMap:
+    charwidth = glyphquery.width(font, glyph_name)
+    if charwidth == 0 or glyph_name not in charmap:
         continue
-    char = unichr(charMap[glyph_name])
+    char = unichr(charmap[glyph_name])
     print(char)
 
     # Make glyph info
-    charSize = (charWidth, charHeight)
-    beziers = fetchBeziers(contours, charSize)
-    grid = makeGrid(beziers)
-    glyphInfos.append({
+    charsize = (charwidth, charheight)
+    beziers = fetch_beziers(contours, charsize)
+    grid = make_grid(beziers)
+    glyph_infos.append({
         'char': char,
         'grid': grid,
         'beziers': beziers,
-        'size': charSize,
+        'size': charsize,
     })
 
     # Statistic
@@ -237,103 +174,77 @@ print
 print(stat)
 print
 print
-print('Glyph count:', len(glyphInfos))
+print('Glyph count:', len(glyph_infos))
 print
 
-
-def copyGrid(grid, atlas, coord):
-    if len(grid) + coord[0] >= len(atlas) or len(grid) + coord[1] >= len(atlas[0]):
-        print(len(grid), len(atlas), coord)
-        raise 'Grid exceeded atlas'
-    gridSize = len(grid)
-    for i in range(gridSize):
-        for j in range(gridSize):
-            cell = grid[gridSize - 1 - i][gridSize - 1 - j]
-            while len(cell) < 4:
-                cell.append(0)
-            atlas[coord[0] + j][coord[1] + i] = cell
-
-def copyMatrix(matrix, atlas, coord):
-    for r in range(len(matrix)):
-        for c in range(len(matrix[r])):
-            atlas[coord[0] + c][coord[1] + r] = matrix[r][c]
-
-def initAtlas(width, height):
-    return [[[0,0,0,0] for i in range(height)] for j in range(width)]
-
-def compressBeziers(beziers):
-    points = [beziers[0][0]]
-    for b in beziers:
-        points.append(b[1])
-        points.append(b[2])
-    return points
-
-def convertShortToBytes(x):
-    return [int(x) / 256, int(x) % 256]
-
-ATLAS_WIDTH = 256
-ATLAS_HEIGHT = 128
-
-glyphStrokes = []
-glyphCoords = {}
-atlas = initAtlas(ATLAS_WIDTH, ATLAS_HEIGHT)
-glyphGridCoord = [0, 0]
-glyphInfos.sort(key=lambda x: len(x['grid']), reverse=True)
-gridRowHeight = 0
-gridsHeight = 0
-for gi in glyphInfos:
+# `g`` is `glyph`
+# g_strokes -- хранит массив из кортежей вида (glyphchar, glyphinfobytes)
+g_strokes = []
+atlas = [[[0,0,0,0] for i in range(ATLAS_WIDTH)] for j in range(ATLAS_HEIGHT)]
+g_grid_coord = [0, 0]
+glyph_infos.sort(key=lambda x: len(x['grid']), reverse=True)
+current_grids_row_height = 0
+grids_height = 0
+for gi in glyph_infos:
     # Grid
-    gridSize = len(gi['grid'])
-    if gridRowHeight == 0:
-        gridRowHeight = gridSize
-        gridsHeight += gridRowHeight
-    elif glyphGridCoord[0] + gridSize >= ATLAS_WIDTH:
-        glyphGridCoord[0] = 0
-        glyphGridCoord[1] += gridRowHeight
-        gridRowHeight = gridSize
-        gridsHeight += gridRowHeight
-    copyGrid(gi['grid'], atlas, glyphGridCoord)
-    glyphGridCoord[0] += gridSize
+    gridsize = len(gi['grid'])
+    if current_grids_row_height == 0:
+        current_grids_row_height = gridsize
+        grids_height += current_grids_row_height
+    elif g_grid_coord[0] + gridsize >= ATLAS_WIDTH:
+        g_grid_coord[0] = 0
+        g_grid_coord[1] += current_grids_row_height
+        current_grids_row_height = gridsize
+        grids_height += current_grids_row_height
+    copy_grid(gi['grid'], atlas, g_grid_coord)
+    g_grid_coord[0] += gridsize
 
     # Stroke
-    gridMin = copy(glyphGridCoord)
-    rasteredGridMin = [0,0,0,0]
-    gridSizeBytes = convertShortToBytes(gridSize)
-    gridMinX = convertShortToBytes(gridMin[0])
-    print(gridMinX)
-    gridMinBytes = gridMinX + convertShortToBytes(gridMin[1])
-    stroke = (gi['char'], gridMinBytes + rasteredGridMin + gridSizeBytes + [0, 0] + compressBeziers(gi['beziers']))
-    glyphStrokes.append(stroke)
+    gridmin = copy(g_grid_coord)
+    rasteredgridmin_pixel = [0,0,0,0]
+    gridsize_pixel = convert_short_to_bytes(gridsize) + [0, 0]
+    gridmin_pixel = convert_short_to_bytes(gridmin[0]) + convert_short_to_bytes(gridmin[1])
+    beziers_pixels = convert_beziers_to_pixels(gi['beziers'])
+    g_stroke = (gi['char'], [gridmin_pixel, rasteredgridmin_pixel, gridsize_pixel] + beziers_pixels)
+    g_strokes.append(g_stroke)
 
-gridsHeight += gridRowHeight
+print('Grids height', grids_height)
 
-glyphInfoMatrix = []
-row = []
-for s in glyphStrokes:
-    if len(row) + len(s[1]) - 1 >= ATLAS_WIDTH:
-        glyphInfoMatrix.append(row)
-        row = []
-    glyphCoords[s[0]] = [len(row), len(glyphStrokes) - 1]
-    row += s[1]
-copyMatrix(glyphInfoMatrix, atlas, [0, gridsHeight])
-
+# glyphs_info_rows -- вторая половина атласа, которая для каждой глифы хранит инфо о сетке глифы в атласе и координаты безье кривых в сжатом виде
+# glyphs_info_row ~ gridmin | rasteredgridmin | gridsize | compressedbezierpoints
+glyphs_info_rows = []
+# g_coords -- хранит координаты блока с инфо глифы в атласе
+g_coords = {}
+glyphs_info_row = []
+for gs in g_strokes:
+    if len(glyphs_info_row) + len(gs[1]) > ATLAS_WIDTH:
+        if len(glyphs_info_row) == 0:
+            raise Exception('Glyph info row is 0 length! Check {0} with {1} length'.format(gs[0], len(gs[1])))
+        glyphs_info_rows.append(glyphs_info_row)
+        glyphs_info_row = []
+    g_coords[gs[0]] = [len(glyphs_info_row), len(glyphs_info_rows) - 1]
+    glyphs_info_row += gs[1]
+copy_matrix(glyphs_info_rows, atlas, [0, grids_height])
 
 if not os.path.exists(DIST_DIR):
     os.makedirs(DIST_DIR)
 
 with open('{}/glyphCoords.json'.format(DIST_DIR), 'w') as f:
-    json.dump(glyphCoords, f)
+    json.dump(g_coords, f)
 
 with open('{}/glyphInfos.json'.format(DIST_DIR), 'w') as f:
-    json.dump(glyphInfos, f)
+    json.dump(glyph_infos, f)
 
 with open("{}/atlas.bmp".format(DIST_DIR), 'w') as f:
     img = Image.new('RGBA', (ATLAS_WIDTH, ATLAS_HEIGHT), (255, 0, 0, 255))
     pixels = img.load()
-    for i in range(ATLAS_WIDTH):
-        for j in range(ATLAS_HEIGHT):
-            cell = atlas[i][j]
-            cell[3] = 255
-            pixels[i,j] = tuple(cell)
+    for i in range(ATLAS_HEIGHT):
+        for j in range(ATLAS_WIDTH):
+            try:
+                cell = atlas[i][j]
+                cell[3] = 255
+                pixels[j,i] = tuple(cell)
+            except Exception as ex:
+                raise Exception('Exception on ({0},{1})'.format(i,j))
     if '--show' in flags:
         img.show()
---
+++ utils.py
@@ -0,0 +1,137 @@
+# coding=utf-8
+
+from cmath import sqrt
+import math
+
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+    '''
+    Python 2 implementation of Python 3.5 math.isclose()
+    https://hg.python.org/cpython/file/tip/Modules/mathmodule.c#l1993
+    '''
+    # sanity check on the inputs
+    if rel_tol < 0 or abs_tol < 0:
+        raise ValueError("tolerances must be non-negative")
+
+    # short circuit exact equality -- needed to catch two infinities of
+    # the same sign. And perhaps speeds things up a bit sometimes.
+    if a == b:
+        return True
+
+    # This catches the case of two infinities of opposite sign, or
+    # one infinity and one finite number. Two infinities of opposite
+    # sign would otherwise have an infinite relative tolerance.
+    # Two infinities of the same sign are caught by the equality check
+    # above.
+    if math.isinf(a) or math.isinf(b):
+        return False
+
+    # now do the regular computation
+    # this is essentially the "weak" test from the Boost library
+    diff = math.fabs(b - a)
+    result = (((diff <= math.fabs(rel_tol * b)) or
+               (diff <= math.fabs(rel_tol * a))) or
+              (diff <= abs_tol))
+    return result
+
+def normalize_fpoint(fpoint, size):
+    return ((float(fpoint[0][0]) / size[0], float(fpoint[0][1]) / size[1]), fpoint[1])
+
+def is_point_inside(point, rectangle):
+    """
+    @param rectangle (x0,y0,x1,y1) x0,y0 left-bottom. x1,y1 right-top.
+    """
+    return point[0] >= rectangle[0] and point[0] <= rectangle[2] and \
+        point[1] >= rectangle[1] and point[1] <= rectangle[3]
+
+def is_inside_section(t, t1, t2):
+    return t >= t1 and t <= t2
+    
+def fetch_bezier_cross(bezier, componentIndex, componentValue):
+    """
+    B(t) = (1-t)^2*p0 + 2(1-t)t*p1 + t^2*p2
+    B(t) = (p0-2p1+p2)tt + 2(p1-p0)t + p0
+
+    component is x or y.
+    componentIndex == 0 is x.
+    componentIndex == 1 is y.
+
+    Возвращает список из t, в которых кривая безье пересекает прямую x=componentValue (или y=componentValue)
+    @return [int] 
+    """
+    c0 = bezier[0][componentIndex]
+    c1 = bezier[1][componentIndex]
+    c2 = bezier[2][componentIndex]
+    a = c0 - 2 * c1 + c2
+    b = c1 - c0
+    c = c0 - componentValue
+
+    # Решаем уравнение ax**2 + bx + c = 0.
+
+    # Линейная форма
+    if isclose(a, 0):
+        if isclose(b, 0):
+            return []
+        t = c / b / 2
+        return [t] if is_inside_section(t, 0, 1) else []
+    # Квадратичная форма
+    D = b ** 2 - a * c
+    if D < 0:
+        return []
+    t1 = (-b - sqrt(D).real) / a
+    t2 = (-b + sqrt(D).real) / a
+    solution = []
+    if is_inside_section(t1, 0, 1):
+        solution.append(t1)
+    if is_inside_section(t2, 0, 1):
+        solution.append(t2)
+    return solution
+
+def calc_bezier(bezier, t):
+    def calcComponentBezier(c0, c1, c2, t):
+        return (1-t)**2*c0 + 2*(1-t)*t*c1 + t**2*c2
+
+    p0, p1, p2 = bezier
+    return (
+        calcComponentBezier(p0[0], p1[0], p2[0], t),
+        calcComponentBezier(p0[1], p1[1], p2[1], t)
+    )
+
+def is_bezier_crossing_cell(bezier, cell_rectangle):
+    x1 = cell_rectangle[0]
+    y1 = cell_rectangle[1]
+    x2 = cell_rectangle[2]
+    y2 = cell_rectangle[3]
+    x1t = fetch_bezier_cross(bezier, 0, cell_rectangle[0])
+    y1t = fetch_bezier_cross(bezier, 1, cell_rectangle[1])
+    x2t = fetch_bezier_cross(bezier, 0, cell_rectangle[2])
+    y2t = fetch_bezier_cross(bezier, 1, cell_rectangle[3])
+    return is_point_inside(bezier[0], cell_rectangle) or is_point_inside(bezier[2], cell_rectangle)\
+        or any(map(lambda t: is_inside_section(calc_bezier(bezier, t)[1], y1, y2), x1t))\
+        or any(map(lambda t: is_inside_section(calc_bezier(bezier, t)[0], x1, x2), y1t))\
+        or any(map(lambda t: is_inside_section(calc_bezier(bezier, t)[1], y1, y2), x2t))\
+        or any(map(lambda t: is_inside_section(calc_bezier(bezier, t)[0], x1, x2), y2t))
+    
+def avrpoint(p1, p2):
+    x0 = float(p1[0] + p2[0]) / 2
+    y0 = float(p1[1] + p2[1]) / 2
+    return (x0, y0)
+
+def convert_short_to_bytes(x):
+    return [int(x / 256), x % 256]
+
+def compress_beziers(beziers):
+    """The end of i bezier is start of i+1 bezier."""
+    if len(beziers) == 0:
+        return []
+    points = [beziers[0][0]]
+    for b in beziers:
+        points.append(b[1])
+        points.append(b[2])
+    return points
+
+def split(a, n):
+    """
+    ref: https://stackoverflow.com/questions/2130016/splitting-a-list-into-n-parts-of-approximately-equal-length
+    """
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
